c6dca3cce648c0d4ce62728b714d64bd39a9b324
Mohammad_Talha <57532114+talhaansari77@users.noreply.github.com>
2020-12-05 22:45:11 +0500

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 	<!-- Insert to your webpage before the </head> -->
 
 
